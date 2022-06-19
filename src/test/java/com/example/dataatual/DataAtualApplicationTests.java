172f317ea722ef3be2cafa0572bfa556f806a499
Wesley Maia <maia.weol@gmail.com>
2022-06-19 16:20:50 -0700

    iniciando projeto

---
+++ src/test/java/com/example/dataatual/DataAtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataatual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
